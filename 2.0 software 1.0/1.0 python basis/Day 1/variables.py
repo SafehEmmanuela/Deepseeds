ab48577fc5af47f912ea880f177db0ed1109cfa6
Safeh Emma <safehemmanuella@gmail.com>
2025-07-23 14:08:13 +0100

    I am done with variables

---
+++ 2.0 software 1.0/1.0 python basis/Day 1/variables.py
@@ -0,0 +1,54 @@
+# print("Hello Deepseeds")
+# name= "Emma Safeh"
+
+# print("My name is " + name + " and my mum gave me the name " + name +
+#       " because someone she really loved has the name " + name)
+# age= 23
+# height= 3.56
+# isMarried=True
+
+# # snake casing: always use snake casing in python
+# User_name= "Safeh Emma"
+# # camel casing
+# UserNameFrom= "Manjoah"
+# # pascal casing
+# UserNameFrom= "Manjoah"
+
+
+# whole_number= 42
+# decimal_number= 3.14159
+# complex_number= 2+ 3j
+
+
+# greeting= "Hello, World"
+# single_char= "A"
+
+
+# is_sunny= True
+# is_raining=False
+
+# print(type(whole_number))
+# print(type(greeting))
+
+# # variable basic examples
+# # concatenate variables
+# first_name="Emma"
+# last_name= " Safeh"
+# # this is not addition because we are adding names, so this iscalled concatenation
+
+# combine_name=first_name + last_name
+# print("My full names are " + combine_name)
+# print(f"My full names are {combine_name}")
+
+
+
+# # A ban name generator
+
+
+name= input("What is your name?")
+age= input("How old are you?")
+Best_meal= input("What is your best meal?")
+school= input("Which school are you from?")
+Department= input("What departments are you from?")
+Best_friend= input("Who is your best friend?")
+print(f"My name is {name}, I am {age} years old. My favorite meal is {Best_meal}. I'm a  student in {school} from the department of {Department}, and my best friend is {Best_friend}")
