bd3ead2b8acefce559a40e60e306b8797561cd99
Safeh Emma <safehemmanuella@gmail.com>
2025-07-24 13:30:09 +0100

    I have just finished with Cinditionals

--- 2.0 software 1.0/1.0 python basis/Day 2/Conditionals.py
+++ 2.0 software 1.0/1.0 python basis/Day 2/Conditionals.py
@@ -23,18 +23,50 @@
 # else:
 #     print("Wrong command")
 
-#if_else_elif chain
-# exercise
-print("___"*50)
-marks= float(input("Input your grade here:"))
-if marks>= 80:
-    print("You have a A❤️ Grade")
-elif marks>=70:
-    print("You have a B+🦸‍♂️ grade")
-elif marks>=60:
-    print("You have a C😂 grade")
+# #if_else_elif chain
+# # exercise
+# print("___"*50)
+# marks= float(input("Input your grade here:"))
+# if marks>= 80:
+#     print("You have a A❤️ Grade")
+# elif marks>=70:
+#     print("You have a B+🦸‍♂️ grade")
+# elif marks>=60:
+#     print("You have a C😂 grade")
+# else:
+#     marks=50
+#     print(" You have a D😒 grade")
+
+
+# # simple calculator
+# first_number= float(input("Enter first number: "))
+# second_number=float(input("Enter second number: "))
+# operator= input("Enter operator")
+# result=0
+
+# if operator=="+":
+#     result=first_number+second_number
+# elif operator=="-":
+#     result=first_number-second_number
+# elif operator=="*":
+#     result=first_number*second_number
+# elif operator=="/":
+#     if second_number!= 0:
+#         result=first_number/second_number
+#         print(result)
+#     else:
+#         print("Cannot be divided by zero")
+# else:
+#     print("Error") 
+
+
+# Leap year excercise
+# to check if a year is a leap year or not
+
+year=int(input("Enter the year here:"))
+if year %4!=0:
+    if year%100!=0:
+        if year%400!=0:
+            print(f"It is not a leap  year")
 else:
-    marks=50
-    print(" You have a D😒 grade")
-
-
+    print(f"It is a leap year")    
